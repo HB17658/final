4051f93dc91e67870d649196f4f0145699ec3cc6
aikomorisaki <ai.940913@gmail.com>
2023-07-04 09:30:00 +0900

    井上さん修正お願いします！

---
+++ final_project/resources/views/login.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログインフォーム</title>
+    <!-- <link rel="stylesheet" href="./css/style.css"> -->
+</head>
+<body>
+    <!-- ログインフォーム -->
+    <form>
+        <h2>ログイン</h2>
+            <p>ID <input type="text"></p>
+            <p>pass <input type="password"></p>
+            <input type="submit">
+    </form>
+    <!-- 新規ユーザー登録 -->
+    <form>
+        <h2>新規ユーザー登録</h2>
+        <a href="">新規登録</a>
+    </form>
+</body>
+</html>
---
+++ final_project/resources/views/top.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>書籍管理システム</title>
+    <!-- <link rel="stylesheet" href="./css/style.css"> -->
+</head>
+<body>
+    <h2>書籍検索</h2>
+    <!-- 検索フォーム -->
+    <form action="" method="">
+        <input type="text">
+        <input type="submit" value="検索">
+    </form>
+    <!-- トップページ -->
+    <a href="">トップページ</a>
+    <!-- 詳細 -->
+    <a href="">詳細検索</a>
+    <!-- 履歴 -->
+    <a href="">履歴</a>
+    <!-- マイページ -->
+    <a href="">マイページ</a>
+    <!-- ログアウト -->
+    <a href="">ログアウト</a>
+    <!-- ヘルプ -->
+    <a href="">ヘルプ</a>
+
+</body>
+</html>
