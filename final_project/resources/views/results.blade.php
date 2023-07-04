dd0b49bd3a788ef804cbf2c4953c4ddf39cc333f
takashiuematsu <138195640+takashiuematsu@users.noreply.github.com>
2023-07-04 11:03:18 +0900

    table,aタグを中央揃え

--- final_project/resources/views/results.blade.php
+++ final_project/resources/views/results.blade.php
@@ -6,28 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>書籍一覧ページ検索結果</title>
 </head>
-
+<style>
+    #table{
+        margin: auto;
+    }
+</style>
 <body>
+<div style="text-align: center;">
     <h1>書籍一覧検索結果ページ</h1>
-
-
     <a href="">トップページに戻る</a>
     <a href="">マイページ</a>
     <a href="">詳細</a>
     <a href="">履歴</a>
     <a href="">ヘルプ</a>
     <a href="">ログアウト</a>
+    <br>
+    <br>
     <form action="" method="">
         <input type="text" name="">
         <input type="submit" value="検索">
     </form>
-
+    </div>
     <br>
     <br>
-
-    <div>
         @foreach
-        <table border="1">
+        <table id="table" border="1">
             
             <tr>
                 <th rowspan="5">
@@ -49,8 +52,6 @@
                 <td>貸出可否</td>
                 <td></td>
             </tr>
-
         </table>
-    </div>
 
 </html>
