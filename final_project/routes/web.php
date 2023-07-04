2b0709f9e6cf67c4466f79319fe78a3b3d0e66a3
muramatsu12345 <138187531+muramatsu12345@users.noreply.github.com>
2023-07-04 10:51:19 +0900

    dだｗ

--- final_project/routes/web.php
+++ final_project/routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+Route::get('/sample',function(){
+    return view('sample');
+});
 Route::get('/createUser',function(){
     return view('createUser');
 });
