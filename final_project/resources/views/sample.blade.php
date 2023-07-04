9a3a20e4ea213258cb723de5ea3fb891566df6ad
muramatsu12345 <138187531+muramatsu12345@users.noreply.github.com>
2023-07-04 11:02:35 +0900

    baseの変更

--- final_project/resources/views/sample.blade.php
+++ final_project/resources/views/sample.blade.php
@@ -8,13 +8,18 @@
     <title>@yield('title')</title>
 </head>
 <body>
-    <h2>書籍表示</h2>
-    <a href="" class="menu">トップページ</a>
-    <a href="" class="menu">詳細検索</a>
-    <a href="" class="menu">履歴</a>
-    <a href="" class="menu">マイページ</a>
-    <a href="" class="menu">ログアウト</a>
-    <a href="" class="menu">ヘルプ</a>
+    <h2 class="title">書籍表示</h2>
+    <nav>
+        <ul>
+            <li><a href="" class="menu">トップページ</a></li>
+            <li><a href="" class="menu">詳細検索</a></li>
+            <li><a href="" class="menu">履歴</a></li>
+            <li><a href="" class="menu">マイページ</a></li>
+            <li><a href="" class="menu">ログアウト</a></li>
+            <li><a href="" class="menu">ヘルプ</a></li>
+        </ul>
+    </nav>
+
     <form action="" method=""  class="form">
         @csrf
         {{-- 本の検索 --}}
