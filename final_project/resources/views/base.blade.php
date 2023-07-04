6617c470fbef3d3c93d52f1f448266a8956e3c38
muramatsu12345 <138187531+muramatsu12345@users.noreply.github.com>
2023-07-04 10:17:00 +0900

    css

--- final_project/resources/views/base.blade.php
+++ final_project/resources/views/base.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+
+    <!--css-->
+
 </head>
 <body>
     <h2>書籍検索</h2>
