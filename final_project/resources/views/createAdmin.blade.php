adf404b8a3b5be33fdd4d7113910efa2395d9105
HB17658 <inoue.reo15@gmail.com>
2023-07-04 14:27:21 +0900

    a

--- final_project/resources/views/createAdmin.blade.php
+++ final_project/resources/views/createAdmin.blade.php
@@ -2,38 +2,35 @@
 @section('title', '新規管理者登録')
 @section('main')
 
-    <h2>新規管理者登録</h2>
-    <table border="1">
-        <tr>
-            <td>ID</td>
-            <td>
-                <form action="">
-                    <input type="text" required>
-                </form>
-            </td>
-        </tr>
-        <tr>
-            <td>PASS</td>
-            <td>
-                <form action="">
-                    <input type="text" required>
-                </form>
-            </td>
-        </tr>
-        <tr>
-            <td>氏名</td>
-            <td>
-                <form action="">
-                    <input type="text" required>
-                </form>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <form action="">
-                    <input type="submit" value="登録">
-                </form>
-            </td>
-        </tr>
-    </table>
+<h2>新規管理者登録</h2>
+<table border="1">
+    <tr>
+        <td>ID</td>
+        <td>
+            <form action="">
+                <input type="text" required>
+            </form>
+        </td>
+    </tr>
+    <tr>
+        <td>PASS</td>
+        <td>
+            <form action="">
+                <input type="text" required>
+            </form>
+        </td>
+    </tr>
+    <tr>
+        <td>氏名</td>
+        <td>
+            <form action="">
+                <input type="text" required>
+            </form>
+        </td>
+    </tr>
+
+</table>
+<form action="">
+    <input type="submit" value="登録">
+</form>
 @endsection
