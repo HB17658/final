16452909e46226a1735a3b4e3b286871eb4fd559
HB17658 <inoue.reo15@gmail.com>
2023-07-04 10:12:06 +0900

    修正

--- final_project/resources/views/base.blade.php
+++ final_project/resources/views/base.blade.php
@@ -2,11 +2,6 @@
 @section('title', '本の概要ページ')
 @section('main')
     <h2>書籍検索</h2>
-    <a href="" class="menu">トップページ</a>
-    <a href="" class="menu">履歴</a>
-    <a href="" class="menu">マイページ</a>
-    <a href="" class="menu">ログアウト</a>
-    <a href="" class="menu">ヘルプ</a>
     <form action="" method="" class="form">
         @csrf
         {{-- 本の検索 --}}
--- final_project/resources/views/results.blade.php
+++ final_project/resources/views/results.blade.php
@@ -3,47 +3,35 @@
 @section('main')
     <h1>
         書籍一覧検索結果ページ</h1>
-
-
-    <a href="">トップページに戻る</a>
-    <a href="">マイページ</a>
-    <a href="">詳細</a>
-    <a href="">履歴</a>
-    <a href="">ヘルプ</a>
-    <a href="">ログアウト</a>
     <form action="" method="">
         <input type="text" name="">
         <input type="submit" value="検索">
     </form>
 
-    <br>
-    <br>
+    @foreach
+    <table>
+        <tr>
+            <th rowspan="5">
+                <a href=""><img src="" alt=""></a>
+            <td>タイトル</td>
 
-    <div>
-        @foreach
-        <table border="1">
+            <td><a href="">タイトル</a></td>
+            </th>
+        </tr>
+        <tr>
+            <td>作者</td>
+            <td>
+            </td>
+        </tr>
+        <tr>
+            <td>カテゴリー</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>貸出可否</td>
+            <td></td>
+        </tr>
 
-            <tr>
-                <th rowspan="5">
-                    <a href=""><img src="../..///" alt=""></a>
-                <td>タイトル</td>
-
-                <td><a href="">あああああ</a></td>
-                </th>
-            </tr>
-            <tr>
-                <td>作者</td>
-                <td>@foreach @endforeach
-                </td>
-            </tr>
-            <tr>
-                <td>カテゴリー</td>
-                <td></td>
-            </tr>
-            <tr>
-                <td>貸出可否</td>
-                <td></td>
-            </tr>
-
-        </table>
-    @endsection
+    </table>
+    @endforeach
+@endsection
