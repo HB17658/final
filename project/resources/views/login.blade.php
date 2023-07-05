ba977597b1ac5d9ab629181ef54b87cc7dd03bf6
HB17658 <inoue.reo15@gmail.com>
2023-07-05 10:47:52 +0900

    変更・追加

---
+++ project/resources/views/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログインフォーム</title>
+    <!-- <link rel="stylesheet" href="./css/style.css"> -->
+</head>
+<body>
+    <!-- ログインフォーム -->
+    <form action="/top" method="POST">
+        @csrf
+        <h2>ログイン</h2>
+            <p>ID <input type="text"></p>
+            <p>pass <input type="password"></p>
+            <input type="submit">
+    </form>
+    
+</body>
+</html>
