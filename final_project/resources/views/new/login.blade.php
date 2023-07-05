a949d91bd6177d2a48f1f67a91790c7dbfdc4510
muramatsu12345 <138187531+muramatsu12345@users.noreply.github.com>
2023-07-05 14:33:42 +0900

    css

--- final_project/resources/views/new/login.blade.php
+++ final_project/resources/views/new/login.blade.php
@@ -13,8 +13,7 @@
     <h1 class="title">書籍検索</h1>
     <h2 class="subTitle">ログインフォーム</h2>
         <form action="new/result" method="post">
-    <h2>ログインフォーム</h2>
-        <form action="/new/result" method="post">
+
         @csrf
             <p>ログインID</p>
             <input type="text" name="id" placeholder="ログインIDを入力して下さい" required>
