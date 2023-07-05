0c684df0fac64c12107c9debe122dbc50a29286c
aikomorisaki <ai.940913@gmail.com>
2023-07-05 15:15:13 +0900

    fhg

--- final_project/resources/views/new/login.blade.php
+++ final_project/resources/views/new/login.blade.php
@@ -4,19 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="../../../css/login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
 
     <h1 class="title">書籍検索</h1>
     <h2 class="subTitle">ログインフォーム</h2>
-        <form action="new/result" method="post">
+        <form class="form" action="new/result" method="post">
 
         @csrf
-            <p>ログインID</p>
+            <p class="item">ログインID</p>
             <input type="text" name="id" placeholder="ログインIDを入力して下さい" required>
-            <p>パスワード</p>
+            <p class="item">パスワード</p>
             <input type="password" name="password" placeholder="パスワードを入力して下さい" required> 
             <input type="submit" class="loginBtn" value="ログイン">
         </form>
