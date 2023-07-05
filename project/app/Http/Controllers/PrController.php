cbc2ce4712a24a15182487a076ea885e9404fcd2
GOTOBUNNOHANAYOME <138414720+GOTOBUNNOHANAYOME@users.noreply.github.com>
2023-07-05 11:27:51 +0900

    afee

--- project/app/Http/Controllers/PrController.php
+++ project/app/Http/Controllers/PrController.php
@@ -15,7 +15,7 @@ class PrController extends Controller
         //最終的にレコードをview(result.blade.php)に返す
         //return view('result',$data);
 
-
+        
         //入力された文字列と一致する書籍情報をすべて取得し1つずつ取得し、連想配列に入れる。
         $searchData = [];
         //retrunでは$searchDataを返す
