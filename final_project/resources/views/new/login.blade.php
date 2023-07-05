1fcba69c03352936ad2c998f7e0ffde7c69be94a
aikomorisaki <ai.940913@gmail.com>
2023-07-05 14:36:45 +0900

    hgf

--- final_project/resources/views/new/login.blade.php
+++ final_project/resources/views/new/login.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/login.css">
-    <link rel="stylesheet" href="css/base.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
@@ -18,7 +17,7 @@
             <input type="text" name="id" placeholder="ログインIDを入力して下さい" required>
             <p>パスワード</p>
             <input type="password" name="password" placeholder="パスワードを入力して下さい" required> 
-            <input type="submit" value="ログイン">
+            <input type="submit" class="loginBtn" value="ログイン">
         </form>
 
 </body>
