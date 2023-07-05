cea8437094a4bcf43b87d675435486533ed2a241
GOTOBUNNOHANAYOME <138414720+GOTOBUNNOHANAYOME@users.noreply.github.com>
2023-07-05 13:14:37 +0900

    dwaadaw

--- final_project/app/Http/Controllers/PrController.php
+++ final_project/app/Http/Controllers/PrController.php
@@ -8,24 +8,47 @@ class PrController extends Controller
 {
     public function search(Request $r)
     {
-        
+        $id = $r->id;
+        $password = $r->password;
+        //DBから社員データを抽出
+        //$empData = 
+        if(isset($id) && isset($password)){
+            
+            //database内にあるすべてのid&passが一致するかを検証
+            // if($id == $empData->id && $password == $empData->password){
+                    //社員情報が一致しなかった場合前の画面に戻す。
+                    //redirect()-back();
+           // }
+
+            //セッションにidやpasswordを登録
+            $r->session()->put('id',$id);
+            $r->session()->put('password',$password);
+        }
+        $id = $r->session()->get('id');
+        $password = $r->session()->get('password');
+
+
         //一覧表示するためにデータベースから書籍情報をすべて取得し1つずつレコードを連想配列に入れる
         //$data = [];
         //最終的にレコードをview(result.blade.php)に返す
         //return view('result',$data);
+        
 
+        
 
         //入力された文字列と一致する書籍情報をすべて取得し1つずつ取得し、連想配列に入れる。
         //requestオブジェクトからsearchwordを抽出
-
+        $searchword = $r->searchword;
         //データベースから書籍をすべて抽出
 
         //一致するキーワードを検索
+        // for($db as $data){
 
+        // }
         //一致したキーワードがある書籍情報を連想配列に格納
-        $searchData = [];
+        //$searchData = [];
         //retrunでは$searchDataを返す
-        return view('new/result',$searchData);
+        //return view('new/result',$searchData);
 
     }
 }
