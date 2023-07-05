31f92f14b75ad5f1b47d36985231f73ee4e68f75
GOTOBUNNOHANAYOME <138414720+GOTOBUNNOHANAYOME@users.noreply.github.com>
2023-07-05 11:08:31 +0900

    fa

--- project/app/Http/Controllers/PrController.php
+++ project/app/Http/Controllers/PrController.php
@@ -7,4 +7,12 @@ use Illuminate\Http\Request;
 class PrController extends Controller
 {
     //
+    public function search(Request $r)
+    {
+        
+        //一覧表示するためにデータベースから書籍情報をすべて取得し1つずつレコードを連想配列に入れる
+        $data = [];
+        //最終的にレコードをview(result.blade.php)に返す
+        return view('result',$data);
+    }
 }
