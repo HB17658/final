7c745269360dea9675b4b04e357573c205bf8042
muramatsu12345 <138187531+muramatsu12345@users.noreply.github.com>
2023-07-05 15:07:54 +0900

    hutter link

--- final_project/resources/views/new/login.blade.php
+++ final_project/resources/views/new/login.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../../css/login.css">
-    <link rel="stylesheet" href="css/base.css">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="/css/login.css">
+    <!-- <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/login.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
@@ -22,6 +22,11 @@
             <input type="password" name="password" placeholder="パスワードを入力して下さい" required> 
             <input type="submit" class="loginBtn" value="ログイン">
         </form>
-
+<!-- フッター -->
+<footer>
+        <div>
+            <p><small>&copy;2023 ICTエンジニア科</small></p>
+        </div>
+    </footer>
 </body>
 </html>
