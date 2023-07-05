97f46371e04b5a7c4998e800cf9b23471b63e7fb
muramatsu12345 <138187531+muramatsu12345@users.noreply.github.com>
2023-07-05 14:01:37 +0900

    css

--- final_project/resources/views/new/result.blade.php
+++ final_project/resources/views/new/result.blade.php
@@ -4,15 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../../../css/base.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <h1>書籍検索</h1>
+<div id="new">
+    <h1 class="title">書籍検索</h1>
     <form action="/new/result" method="post" >
     @csrf
+    <div class="text-center">
     <p>
-        <input type="text" name="searchword" placeholder="検索キーワードを入力してください">
-        <input type="submit" value="検索">
+        <input type="text" name="searchword" class="textbox" placeholder="検索キーワードを入力してください">
+        <input type="submit" value="検索" class="searchbtn">
     </p>
+    </div>
+    </div>
+    
     </form>
     @if(isset($data))
         <h2>書籍一覧</h2>
@@ -25,9 +32,11 @@
                 <!-- 検索フォームから入力された値から一致した書籍一覧を表示するHTML欄 -->
             @endforeach
     @endif
-
-    @if(isset($id))
-    <p>{{$id}}</p>
-    @endif
+    <!-- フッター -->
+    <footer>
+        <div>
+            <p><small>&copy;2023 ICTエンジニア科</small></p>
+        </div>
+    </footer>
 </body>
 </html>
