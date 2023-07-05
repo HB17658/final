3b663903676ec43dc17a8ac9c0aa3b198898367d
GOTOBUNNOHANAYOME <138414720+GOTOBUNNOHANAYOME@users.noreply.github.com>
2023-07-05 11:31:24 +0900

    dwadadwa

--- final_project/routes/web.php
+++ final_project/routes/web.php
@@ -15,38 +15,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('login');
+
+
+Route::get('/',function(){
+    return view('new/login');
 });
-Route::get('/sample',function(){
-    return view('sample');
-});
-Route::get('/createUser',function(){
-    return view('createUser');
-});
-Route::post('/createUser',[DbController::class,'createUser']);
-Route::get('/login',function(){
-    return view('login');
-});
-Route::post('/top',[DbController::class,'login']);
-Route::get('/search',function(){
-    return view('search');
-});
-Route::get('/top',function(){
-    return view('top');
-});
-Route::get('/mypage',function(){
-    return view('mypage');
-});
-Route::post('/results',[DbController::class,'results']);
-Route::get('/addbook',function(){
-    return view('addbook');
-});
-Route::post('/addbook',[DbController::class,'addbook']);
-Route::get('/userResult',function(){
-    return view('userResult');
-});
-Route::post('/userResult',[DbController::class,'userDelete']);
-Route::get('/createAdmin',function(){
-    return view('createAdmin');
-});
+
+
+
+
+//ボツ
+// Route::get('/', function () {
+//     return view('login');
+// });
+// Route::get('/sample',function(){
+//     return view('sample');
+// });
+// Route::get('/createUser',function(){
+//     return view('createUser');
+// });
+// Route::post('/createUser',[DbController::class,'createUser']);
+// Route::get('/login',function(){
+//     return view('login');
+// });
+// Route::post('/top',[DbController::class,'login']);
+// Route::get('/search',function(){
+//     return view('search');
+// });
+// Route::get('/top',function(){
+//     return view('top');
+// });
+// Route::get('/mypage',function(){
+//     return view('mypage');
+// });
+// Route::post('/results',[DbController::class,'results']);
+// Route::get('/addbook',function(){
+//     return view('addbook');
+// });
+// Route::post('/addbook',[DbController::class,'addbook']);
+// Route::get('/userResult',function(){
+//     return view('userResult');
+// });
+// Route::post('/userResult',[DbController::class,'userDelete']);
+// Route::get('/createAdmin',function(){
+//     return view('createAdmin');
+// });
