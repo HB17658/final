ace487db46ad27d5e04902835f4135d62d4e5bd4
muramatsu12345 <138187531+muramatsu12345@users.noreply.github.com>
2023-07-06 09:33:39 +0900

    btn

--- final_project/resources/views/new/result.blade.php
+++ final_project/resources/views/new/result.blade.php
@@ -10,7 +10,7 @@
 <body>
 <div id="new">
     <h1 class="title">書籍検索</h1>
-    <a href="/" class="">ログアウト</a>
+    <a href="/" class="btn btn--orange">ログアウト</a>
     <form action="/new/result" method="post" >
     @csrf
     <div class="text-center">
