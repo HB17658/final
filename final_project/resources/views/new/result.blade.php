8afb5a6a4a073232f4539ffec2b02337db9fd923
muramatsu12345 <138187531+muramatsu12345@users.noreply.github.com>
2023-07-06 11:04:28 +0900

    css

--- final_project/resources/views/new/result.blade.php
+++ final_project/resources/views/new/result.blade.php
@@ -27,6 +27,13 @@
     <p>{{$msg}}</p>
     @endif
     @if(isset($results))
+    
+        <!-- タイトルヘッダー -->
+        <header class="post-info">
+            <h2 class="post-title">書籍の詳細</h2>
+            <p class="post-data">書籍 <span>一覧</span></p>
+        </header>
+    
         <h2>書籍一覧</h2>
         @foreach($results as $result)
                 <!-- ログインした直後に書籍一覧を表示するHTML欄 -->
@@ -48,6 +55,7 @@
             <td></td>
         </tr>
     </table>
+    
 
         @endforeach
     @elseif(isset($searchData))
