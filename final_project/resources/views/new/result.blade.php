008ff59fcbf2cf244377dd1c603522983513075f
muramatsu12345 <138187531+muramatsu12345@users.noreply.github.com>
2023-07-06 13:00:14 +0900

    css

--- final_project/resources/views/new/result.blade.php
+++ final_project/resources/views/new/result.blade.php
@@ -8,9 +8,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
+    <!-- ヘッダー検索 -->
 <div id="new">
     <h1 class="title">書籍検索</h1>
-    <p class="right"><a href="/" class="btn btn--orange" >ログアウト</a></p>
+    <p class="right"><a href="/" class="btn btn--orange" >ログアウト⇒</a></p>
     <form action="/new/result" method="post" >
     @csrf
     <div class="text-center">
@@ -36,12 +37,12 @@
         
         <div class = "content-wrapper">
             @foreach($results as $result)
-                        <!-- ログインした直後に書籍一覧を表示するHTML欄 -->
-                        <table>
-                <tr><th colspan="3">{{$result->title}}</th></tr>
+            <!-- ログインした直後に書籍一覧を表示するHTML欄 -->
+            <table>
+                <tr><th colspan="3" class="bookTitle">{{$result->title}}</th></tr>
                 <tr><th colspan="3">{{$result->author_name}}</th></tr>
                 <tr>
-                    <td rowspan="4"><img src="{{ asset($result->image)}}" alt="50" height="200" width="150"></td>
+                    <td rowspan="4"><img src="{{ asset($result->image)}}" alt="50" height="250" width="250"></td>
                 </tr>
                 <tr>
                 <td>本の概要</td>
@@ -50,12 +51,18 @@
                     <td>{{$result->info}}</td>
                 </tr>
                 
-                </table>
+            </table>
 
             @endforeach
         </div>
     @elseif(isset($searchData))
-        <h2>検索結果一覧</h2>
+        <!-- タイトルヘッダー -->
+        <header class="post-info">
+            <h2 class="post-title">検索の詳細</h2>
+            <p class="post-data">検索 <span>一覧</span></p>
+        </header>
+
+        <!-- <h2>検索結果一覧</h2> -->
         <div class = "content-wrapper">
             @foreach($searchData as $r)
                     <!-- 検索フォームから入力された値から一致した書籍一覧を表示するHTML欄 -->
