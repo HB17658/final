c513c732d2bbe293cacd8e5212768baae0902347
muramatsu12345 <138187531+muramatsu12345@users.noreply.github.com>
2023-07-07 14:30:27 +0900

    url

--- final_project/resources/views/new/result.blade.php
+++ final_project/resources/views/new/result.blade.php
@@ -150,15 +150,15 @@
             <aside><!-- サブ部分 -->
                 <h3 class="sub-title">カテゴリー</h3>
                 <ul>
-                    <li><a href="">本の紹介</a></li>
-                    <li><a href="">おすすめ１</a></li>
-                    <li><a href="">おすすめ２</a></li>
-                    <li><a href="">おすすめ３</a></li>
+                    <li><a href="https://store.kinokuniya.co.jp/event/kinobest2023/">本の紹介</a></li>
+                    <li><a href="https://amzn.asia/d/d49KpZK">おすすめ１</a></li>
+                    <li><a href="https://amzn.asia/d/6lm1DY4">おすすめ２</a></li>
+                    <li><a href="https://amzn.asia/d/iSeHrCz">おすすめ３</a></li>
                 </ul>
 
                 <h3 class="sub-title">書籍紹介動画</h3>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/WGZ2Qm_NcEY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/GMMtuR2jrm8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </aside>
         </div>
     @endif
